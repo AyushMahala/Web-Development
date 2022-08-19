3e15d2b76ca1bd680efbbfdc3e267d2f1766be17
AyushMahala <81615691+AyushMahala@users.noreply.github.com>
2022-08-19 10:47:31 +0530

    Add files via upload

---
+++ Javascript task1/index.js
@@ -0,0 +1,44 @@
+
+let myVar=("Hello World Welcome!");
+console.log(typeof("hello"));
+console.log(typeof(true));
+console.log(myVar.length);
+console.log(myVar.charAt(10));
+console.log(myVar.indexOf("lc"));
+console.log(myVar.slice(6,7));
+console.log(myVar.toUpperCase());
+console.log(myVar.toLowerCase());
+console.log(myVar.includes("World")); 
+
+// var than50=prompt("Enter a number: ");
+
+// if(than50==50){
+//     alert("Number is equal to 50");
+// }
+// else if(than50<50){
+//     alert("Number is less than 50");
+// }
+// else{
+//     alert("Number is greater than 50");
+// }
+
+// var num1=prompt("Enter first number: ");
+// var num2=prompt("Enter second number: ");
+// if(num1==num2){
+//     alert("Both are equal.");
+// }
+// else if(num1<num2){
+//     alert(num1+" is less than "+num2);
+// }
+// else{
+//     alert(num1+" is greater than "+num2);
+// }
+
+// for(var i=0;i<5;i++){
+//     document.write(i+1+"<br>");
+// }
+var j=0;
+while(j<10){
+    document.write(j+1+"<br>");
+    j++;
+}
